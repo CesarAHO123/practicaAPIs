d3ccac64b0e4415aa8e303d1050344dff4668d20
Cesar Hdez <alejandroho23@hotmail.com>
2021-05-28 19:16:15 -0500

    Conexion con postgresql funcional

--- app.js
+++ app.js
@@ -1,59 +1,63 @@
-const express  = require('express'); 
-const mysql= require('mysql');
-const bodyParser = require ('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 const e = require('express');
 const PORT = process.env.PORT || 3306;
 const app = express();
 app.use(bodyParser.json());
+const { Client } = require('pg');
 
 
-
-
-//Conexion de MySQL
-const connection = mysql.createConnection({
-    host: 'localhost',
+//Datos de la conexion a postgresql
+const connectionData = {
     user: 'postgres',
-    password: '270201', 
-    database: 'node20_mysql'
-}); 
+    host: 'localhost',
+    database: 'node20_mysql',
+    password: '270201',
+    port: 5432,
+
+};
+const client = new Client(connectionData);
 //Route
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.send('Test de la API!');
 });
 // Todas las mascotas
-app.get('/pets', async (req,res) => {
-    const sql= 'SELECT * FROM pets';
-    connection.query(sql, (error, results) => {
-        if (error) throw error;
-        if (results.length > 0) {
-          res.json(results);
-        } else {
-          res.send('Not result');
-        }
-      })
+app.get('/pets', (req, res) => {
+    client.connect()
+    client.query('SELECT * FROM pets')
+        .then(results => {
+            if (results.length > 0) {
+                res.json(results);
+            } else {
+                res.send('Not result');
+            }
+        })
+        .catch(err => {
+            client.end()
+        })
+   
 });
 //Agregamos mascotas
-app.post('/add',(req,res) => {
- res.send('New Pet')
+app.post('/add', (req, res) => {
+    res.send('New Pet')
 });
 //Modificamos los datos de las mascotas
-app.put('/update/:id', (req,res) => {
+app.put('/update/:id', (req, res) => {
     res.send('Update Pet');
 })
 //Eliminamos una mascota
-app.delete('/delete/:id', (req,res) => {
- res.send('Delete pet');
+app.delete('/delete/:id', (req, res) => {
+    res.send('Delete pet');
 });
 // Mascotas por id
-app.get('/pets/:id',(req,res) => {
+app.get('/pets/:id', (req, res) => {
     res.send('Get pets by id');
 });
 
- // Comprobamos la conexion
-connection.connect(error =>
-    {
-        if (error) throw error; 
-        console.log('Database server running!');
-    }
-    );
-    app.listen(PORT,()=>console.log('Server running on port ${PORT}'));
+/* Comprobamos la conexion
+connection.connect(error => {
+    if (error) throw error;
+    console.log('Database server running!');
+}
+);*/
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
